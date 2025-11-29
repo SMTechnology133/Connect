2af0e74e5257249a76688ee06d6c18dbefdce296
SMTechnology133 <salmandac133@gmail.com>
2025-11-29 22:10:10 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,60 +4,80 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e8 });
 
-// Serve static files with UTF-8 headers
-app.use(express.static("public", {
-    setHeaders: (res, path) => {
-        if (path.endsWith(".html")) {
-            res.setHeader("Content-Type", "text/html; charset=utf-8");
-        }
-        if (path.endsWith(".css")) {
-            res.setHeader("Content-Type", "text/css; charset=utf-8");
-        }
-        if (path.endsWith(".js")) {
-            res.setHeader("Content-Type", "application/javascript; charset=utf-8");
-        }
-        if (path.endsWith(".json")) {
-            res.setHeader("Content-Type", "application/json; charset=utf-8");
-        }
-    }
-}));
+const io = new Server(server, {
+    maxHttpBufferSize: 1e8,
+    cors: { origin: "*" }
+});
+
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
-io.on('connection', (socket) => {
-  console.log('connect', socket.id);
+/* socketId → { name, avatar } */
+let users = {};
 
-  socket.on('join', (user) => {
-    socket.data.user = user;
-    socket.broadcast.emit('user-joined', { id: socket.id, user });
-  });
+function broadcastUsers() {
+    io.emit("users-list", users);
+}
 
-  socket.on('typing', (payload) => {
-    socket.broadcast.emit('typing', { id: socket.id, user: socket.data.user, ...payload });
-  });
+io.on("connection", socket => {
+    console.log("connected:", socket.id);
 
-  socket.on('stop-typing', (payload) => {
-    socket.broadcast.emit('stop-typing', { id: socket.id, user: socket.data.user, ...payload });
-  });
+    socket.emit("users-list", users);
 
-  socket.on('chat-message', (msg) => {
-    socket.broadcast.emit('chat-message', msg);
-  });
+    /* JOIN */
+    socket.on("join", ({name,avatar})=>{
+        socket.data.user = { name, avatar };
+        users[socket.id] = socket.data.user;
 
-  socket.on('file-message', (payload) => {
-    socket.broadcast.emit('file-message', payload);
-  });
+        socket.broadcast.emit("user-joined", {
+            id: socket.id,
+            user: socket.data.user
+        });
 
-  socket.on('message-read', (payload) => {
-    socket.broadcast.emit('message-read', payload);
-  });
+        broadcastUsers();
+    });
 
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-left', { id: socket.id, user: socket.data.user });
-    console.log('disconnect', socket.id);
-  });
+    /* PROFILE UPDATE */
+    socket.on("update-profile", ({name,avatar})=>{
+        socket.data.user = { name, avatar };
+        users[socket.id] = socket.data.user;
+        io.emit("profile-updated", { id: socket.id, user: socket.data.user });
+        broadcastUsers();
+    });
+
+    /* TYPING */
+    socket.on("typing", ()=>{
+        socket.broadcast.emit("typing", { user: socket.data.user });
+    });
+
+    socket.on("stop-typing", ()=>{
+        socket.broadcast.emit("stop-typing");
+    });
+
+    /* MESSAGES */
+    socket.on("chat-message", msg=>{
+        socket.broadcast.emit("chat-message", msg);
+    });
+
+    socket.on("file-message", payload=>{
+        socket.broadcast.emit("file-message", payload);
+    });
+
+    socket.on("message-read", data=>{
+        socket.broadcast.emit("message-read", data);
+    });
+
+    /* DISCONNECT */
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("user-left", {
+            id: socket.id,
+            user: socket.data.user || {name:"Unknown"}
+        });
+        delete users[socket.id];
+        broadcastUsers();
+    });
 });
 
-server.listen(PORT, () => console.log('Server running on port', PORT));
+server.listen(PORT, ()=>console.log("Server running on", PORT));
