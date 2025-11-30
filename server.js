683d8ae5fa5e3cce16bbb2700baa5687fb319819
SMTechnology133 <salmandac133@gmail.com>
2025-11-30 21:29:04 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
-/* socketId → { name, avatar } */
+/* socketId → {name, avatar} */
 let users = {};
 
 function broadcastUsers() {
@@ -27,7 +27,7 @@ io.on("connection", socket => {
     socket.emit("users-list", users);
 
     /* JOIN */
-    socket.on("join", ({name,avatar})=>{
+    socket.on("join", ({ name, avatar }) => {
         socket.data.user = { name, avatar };
         users[socket.id] = socket.data.user;
 
@@ -39,45 +39,55 @@ io.on("connection", socket => {
         broadcastUsers();
     });
 
-    /* PROFILE UPDATE */
-    socket.on("update-profile", ({name,avatar})=>{
+    /* UPDATE PROFILE */
+    socket.on("update-profile", ({ name, avatar }) => {
         socket.data.user = { name, avatar };
         users[socket.id] = socket.data.user;
-        io.emit("profile-updated", { id: socket.id, user: socket.data.user });
+
+        io.emit("profile-updated", {
+            id: socket.id,
+            user: socket.data.user
+        });
+
         broadcastUsers();
     });
 
     /* TYPING */
-    socket.on("typing", ()=>{
+    socket.on("typing", () => {
         socket.broadcast.emit("typing", { user: socket.data.user });
     });
 
-    socket.on("stop-typing", ()=>{
+    socket.on("stop-typing", () => {
         socket.broadcast.emit("stop-typing");
     });
 
-    /* MESSAGES */
-    socket.on("chat-message", msg=>{
+    /* TEXT MESSAGE */
+    socket.on("chat-message", msg => {
         socket.broadcast.emit("chat-message", msg);
     });
 
-    socket.on("file-message", payload=>{
+    /* FILE MESSAGE */
+    socket.on("file-message", payload => {
         socket.broadcast.emit("file-message", payload);
     });
 
-    socket.on("message-read", data=>{
+    /* READ RECEIPTS */
+    socket.on("message-read", data => {
         socket.broadcast.emit("message-read", data);
     });
 
     /* DISCONNECT */
-    socket.on("disconnect",()=>{
+    socket.on("disconnect", () => {
         socket.broadcast.emit("user-left", {
             id: socket.id,
-            user: socket.data.user || {name:"Unknown"}
+            user: socket.data.user || { name: "Unknown" }
         });
+
         delete users[socket.id];
         broadcastUsers();
     });
 });
 
-server.listen(PORT, ()=>console.log("Server running on", PORT));
+server.listen(PORT, () =>
+    console.log("Server running on", PORT)
+);
